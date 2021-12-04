a399596c9da370249bfa78332c04507d921bc790
vandergrafgenerator <95262977+vandergrafgenerator@users.noreply.github.com>
2021-12-04 02:30:50 -0300

    Create app.pl

---
+++ frameworks/Perl/plack/plack-starman/app.pl
@@ -0,0 +1,21 @@
+use strict;
+use Unix::Processors;
+use Cwd 'getcwd';
+
+my $cpus = Unix::Processors->new->max_online;
+
+my @cmd = ([qw'plackup -s Starman -E production --port 8080 --workers=', $cpus, qw'-a app.psgi'],
+           [qw'nginx -c nginx.conf -p', getcwd]);
+
+my @child;
+for (@cmd) {
+    if ((my $pid = fork) > 0) { push @child, $pid }
+    elsif (defined $pid) { exec @$_ }
+    else { die 'fork failed' }
+}
+
+# wait childs
+$SIG{INT} = $SIG{TERM} = sub { kill TERM => @child; 1 while wait != -1 };
+1 while wait != -1;
+
+exit 0;
